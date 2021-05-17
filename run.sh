40e25ea84cbc079b80e7f99668860a267d5a6fce
Vitche Research Team Developer <developer@vitche.com>
2021-05-17 19:59:42 +0300

    1. The latest IP-cluster configuration template.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker run -dit --restart always --name python-notebook-timeline -p 8888:8888 -p 9801-9910:9801-9910 --env CHAINCODE_ID_NAME=c46bde49271c45bca1df3ff876e4b2a9 --env CHAINCODE_VERSION=0.0.1 --env PEER_ADDRESS=92.119.223.177:7052 vitche/python-notebook-timeline
+docker run -dit --restart always --name python-notebook-timeline -p 8890:8888 -p 9801-9910:9801-9910 --env CHAINCODE_ID_NAME=c46bde49271c45bca1df3ff876e4b2a9 --env CHAINCODE_VERSION=0.0.1 --env PEER_ADDRESS=92.119.223.177:7052 vitche/python-notebook-timeline
 # Container with a "test" prefix
-# docker run -dit --restart always --name python-notebook-timeline-test -p 8889:8888 -p 9801-9910:9801-9910 --env CHAINCODE_ID_NAME=c46bde49271c45bca1df3ff876e4b2a9 --env CHAINCODE_VERSION=0.0.1 --env PEER_ADDRESS=92.119.223.177:7052 vitche/python-notebook-timeline
+# docker run -dit --restart always --name python-notebook-timeline-test -p 8891:8888 -p 9801-9910:9801-9910 --env CHAINCODE_ID_NAME=c46bde49271c45bca1df3ff876e4b2a9 --env CHAINCODE_VERSION=0.0.1 --env PEER_ADDRESS=92.119.223.177:7052 vitche/python-notebook-timeline
